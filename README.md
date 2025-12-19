1ba00d0c665fd2708ddb8866e7973b4d0392e9ff
turkoscammer <75835023+turkoscammer@users.noreply.github.com>
2025-12-19 19:01:35 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# nORS B*T ALTYAPISI 😹👍
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "",
+    prefix: ".",
+    destek: "https://github.com/turkoscammer",
+    topgg: "https://github.com/turkoscammer",
+    davet: "https://github.com/turkoscammer",
+    website: "https://github.com/turkoscammer"
+}
+
